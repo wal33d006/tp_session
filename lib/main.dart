d55bf25e6a24b50cf9d6d58062d4e57dfbf731dd
wal33d006 <waleed_arshad006@hotmail.com>
2020-06-21 22:10:24 +0500

    Add simple UI and api call.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
-import 'package:tp_session/user_controller.dart';
-import 'package:tp_session/user_model.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,9 +14,9 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.blueGrey,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
@@ -31,9 +32,9 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
-  List<User> _users;
-
   bool _isLoading = false;
+
+  List<User> users = List<User>();
 
   void _incrementCounter() {
     setState(() {
@@ -41,48 +42,185 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  void _populateUsers() async {
+    setState(() {
+      _isLoading = true;
+    });
+    var response = await http.get('https://jsonplaceholder.typicode.com/users');
+    var list = jsonDecode(response.body) as List;
+
+    var userList = list.map((item) => User.fromJson(item)).toList();
+    setState(() {
+      users = userList;
+      _isLoading = false;
+    });
+  }
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    _fetchUsers();
+    _populateUsers();
+    _insertUserToFireStore();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text('MUET Flutter Session'),
       ),
       body: _isLoading
           ? Center(
               child: CircularProgressIndicator(),
             )
           : ListView.builder(
-              itemCount: _users.length,
+              itemCount: users.length,
               itemBuilder: (context, index) {
-                var user = _users[index];
-                return ListTile(
-                  leading: CircleAvatar(
-                    child: Text(user.id.toString()),
+                return Card(
+                  elevation: 4.0,
+                  color: Colors.blueGrey,
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      child: Text(users[index].id.toString()),
+                    ),
+                    title: Text(users[index].name),
+                    subtitle: Text(users[index].email),
+                    trailing: Icon(Icons.perm_camera_mic),
                   ),
-                  title: Text(user.name),
-                  subtitle: Text(user.email),
                 );
               }),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => _incrementCounter(),
+      ),
     );
   }
 
-  void _fetchUsers() async {
-    setState(() {
-      _isLoading = true;
-    });
-    UsersController controller = UsersController();
-    var users = await controller.getUsers();
+  void _insertUserToFireStore() async {
+    Firestore.instance.collection('users').document()
+        .setData({ 'title': 'Hello', 'author': 'waleed' });
+  }
+}
 
-    setState(() {
-      _users = users;
-      _isLoading = false;
-    });
+class User {
+  int id;
+  String name;
+  String username;
+  String email;
+  Address address;
+  String phone;
+  String website;
+  Company company;
+
+  User(
+      {this.id,
+      this.name,
+      this.username,
+      this.email,
+      this.address,
+      this.phone,
+      this.website,
+      this.company});
+
+  User.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    name = json['name'];
+    username = json['username'];
+    email = json['email'];
+    address =
+        json['address'] != null ? new Address.fromJson(json['address']) : null;
+    phone = json['phone'];
+    website = json['website'];
+    company =
+        json['company'] != null ? new Company.fromJson(json['company']) : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['name'] = this.name;
+    data['username'] = this.username;
+    data['email'] = this.email;
+    if (this.address != null) {
+      data['address'] = this.address.toJson();
+    }
+    data['phone'] = this.phone;
+    data['website'] = this.website;
+    if (this.company != null) {
+      data['company'] = this.company.toJson();
+    }
+    return data;
+  }
+}
+
+class Address {
+  String street;
+  String suite;
+  String city;
+  String zipcode;
+  Geo geo;
+
+  Address({this.street, this.suite, this.city, this.zipcode, this.geo});
+
+  Address.fromJson(Map<String, dynamic> json) {
+    street = json['street'];
+    suite = json['suite'];
+    city = json['city'];
+    zipcode = json['zipcode'];
+    geo = json['geo'] != null ? new Geo.fromJson(json['geo']) : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['street'] = this.street;
+    data['suite'] = this.suite;
+    data['city'] = this.city;
+    data['zipcode'] = this.zipcode;
+    if (this.geo != null) {
+      data['geo'] = this.geo.toJson();
+    }
+    return data;
+  }
+}
+
+class Geo {
+  String lat;
+  String lng;
+
+  Geo({this.lat, this.lng});
+
+  Geo.fromJson(Map<String, dynamic> json) {
+    lat = json['lat'];
+    lng = json['lng'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['lat'] = this.lat;
+    data['lng'] = this.lng;
+    return data;
+  }
+}
+
+class Company {
+  String name;
+  String catchPhrase;
+  String bs;
+
+  Company({this.name, this.catchPhrase, this.bs});
+
+  Company.fromJson(Map<String, dynamic> json) {
+    name = json['name'];
+    catchPhrase = json['catchPhrase'];
+    bs = json['bs'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['name'] = this.name;
+    data['catchPhrase'] = this.catchPhrase;
+    data['bs'] = this.bs;
+    return data;
   }
 }
--- test/users_test.dart
+++ test/users_test.dart
@@ -1,14 +1,15 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:tp_session/user_controller.dart';
+import 'dart:convert';
 
+import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart' as http;
+import 'package:tp_session/main.dart';
 
 void main() {
-  test(
-    'Counter increments smoke test',
-    () async {
-      UsersController controller = UsersController();
-      var users = await controller.getUsers();
-      print(users.first.name);
-    },
-  );
-}
+  test('Counter increments smoke test', () async {
+    var response = await http.get('https://jsonplaceholder.typicode.com/users');
+    var list = jsonDecode(response.body) as List;
+
+    var userList = list.map((item) => User.fromJson(item)).toList();
+
+    print(userList.first.name);
+  });}
